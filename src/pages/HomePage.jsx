943447eea82a79c938d3532e0d1d38bcb6cdebec
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-07-28 19:17:24 +0000

    refactor: move handling of side effects to parent
    The mounting and unmounting of HomePage causes some useEffects code to
    run again, when it was just needed to run once, and hence, causing some
    unexpected behaviour.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,65 +1,24 @@
-import { useState, useEffect } from 'react';
-
 import Loading from '../components/ui/Loading';
 import Button from '../components/ui/Button';
 
 import gameLogo from '../assets/images/logo.png';
-import { unwantedPersons } from '../data/unwantedPersons';
 import './HomePage.css';
 
-function filterData(data) {
-  const filteredData = data.filter(
-    (item) =>
-      !unwantedPersons.some(
-        (p) =>
-          p.person === item.person.name && p.character === item.character.name
-      )
-  );
-  return filteredData;
-}
-
 export default function HomePage({
-  allCharacters,
-  setAllCharacters,
+  characters,
   animateIn,
   setAnimateIn,
+  error,
   setRenderHomePage,
-  setDifficultyLevel,
+  setDifficulty,
   setRenderGamePage,
 }) {
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      async function fetchCharacters() {
-        try {
-          const response = await fetch(
-            'https://api.tvmaze.com/shows/55138/cast'
-          );
-
-          if (!response.ok) {
-            throw new Error('Failed to fetch characters.');
-          }
-
-          const data = await response.json();
-          setAllCharacters(filterData(data));
-        } catch (error) {
-          console.error(error); // Do I need to console this?
-          setError(error.message);
-        }
-      }
-
-      fetchCharacters();
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  useEffect(() => {
-    if (allCharacters) {
-      requestAnimationFrame(() => setAnimateIn(true));
-    }
-  }, [allCharacters]);
+  function handleDifficultySelection(e) {
+    setRenderHomePage(false);
+    setAnimateIn(false);
+    setDifficulty(e.target.textContent);
+    setRenderGamePage(true);
+  }
 
   if (error) {
     return (
@@ -72,14 +31,8 @@ export default function HomePage({
     );
   }
 
-  if (!allCharacters) {
+  if (!characters) {
     return <Loading />;
-  }
-
-  function handleDifficultySelection(e) {
-    setRenderHomePage(false);
-    setDifficultyLevel(e.target.textContent);
-    setRenderGamePage(true);
   }
 
   return (
