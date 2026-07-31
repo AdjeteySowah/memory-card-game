84496a32b022bcc79d94bd5a644b309a0e8c07e1
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-07-31 16:34:13 +0000

    feat: add card tilting and 3D flipping animation

--- src/components/game/Card.jsx
+++ src/components/game/Card.jsx
@@ -1,12 +1,21 @@
+import Tilt from 'react-parallax-tilt';
 import './Card.css';
 
-export default function Card({ src, alt, characterName }) {
+export default function Card({ src, alt, characterName, isFlipped }) {
   return (
-    <div className="card">
-      <div className="card-image-container">
-        <img className="card-image" src={src} alt={alt} />
+    <Tilt className="card-tilt" tiltEnable={!isFlipped}>
+      <div className={`card ${isFlipped ? 'flipped' : ''}`}>
+        <div className="card-inner">
+          <div className="card-front">
+            <div className="card-image-container">
+              <img className="card-image" src={src} alt={alt} />
+            </div>
+            <span className="card-character-name">{characterName}</span>
+          </div>
+
+          <div className="card-back"></div>
+        </div>
       </div>
-      <span className="card-character-name">{characterName}</span>
-    </div>
+    </Tilt>
   );
 }
